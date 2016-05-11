1fb3cc0d2fe7708851da7f850d4d973ee4492fd9
czhang <cranezhang@Objectivasoftware.com>
2016-05-11 22:24:16 +0800

    add system configure form

---
+++ MRS/SystemConfigForm.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace MRS
+{
+	partial class SystemConfigForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.Windows.Forms.TreeNode treeNode13 = new System.Windows.Forms.TreeNode("Node4");
+			System.Windows.Forms.TreeNode treeNode14 = new System.Windows.Forms.TreeNode("Node5");
+			System.Windows.Forms.TreeNode treeNode15 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
+            treeNode13,
+            treeNode14});
+			System.Windows.Forms.TreeNode treeNode16 = new System.Windows.Forms.TreeNode("Node6");
+			System.Windows.Forms.TreeNode treeNode17 = new System.Windows.Forms.TreeNode("Node7");
+			System.Windows.Forms.TreeNode treeNode18 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
+            treeNode16,
+            treeNode17});
+			System.Windows.Forms.TreeNode treeNode19 = new System.Windows.Forms.TreeNode("Node8");
+			System.Windows.Forms.TreeNode treeNode20 = new System.Windows.Forms.TreeNode("Node9");
+			System.Windows.Forms.TreeNode treeNode21 = new System.Windows.Forms.TreeNode("Node2", new System.Windows.Forms.TreeNode[] {
+            treeNode19,
+            treeNode20});
+			System.Windows.Forms.TreeNode treeNode22 = new System.Windows.Forms.TreeNode("Node10");
+			System.Windows.Forms.TreeNode treeNode23 = new System.Windows.Forms.TreeNode("Node11");
+			System.Windows.Forms.TreeNode treeNode24 = new System.Windows.Forms.TreeNode("Node3", new System.Windows.Forms.TreeNode[] {
+            treeNode22,
+            treeNode23});
+			System.Windows.Forms.TreeNode treeNode1 = new System.Windows.Forms.TreeNode("Node4");
+			System.Windows.Forms.TreeNode treeNode2 = new System.Windows.Forms.TreeNode("Node5");
+			System.Windows.Forms.TreeNode treeNode3 = new System.Windows.Forms.TreeNode("Node0", new System.Windows.Forms.TreeNode[] {
+            treeNode1,
+            treeNode2});
+			System.Windows.Forms.TreeNode treeNode4 = new System.Windows.Forms.TreeNode("Node6");
+			System.Windows.Forms.TreeNode treeNode5 = new System.Windows.Forms.TreeNode("Node7");
+			System.Windows.Forms.TreeNode treeNode6 = new System.Windows.Forms.TreeNode("Node1", new System.Windows.Forms.TreeNode[] {
+            treeNode4,
+            treeNode5});
+			System.Windows.Forms.TreeNode treeNode7 = new System.Windows.Forms.TreeNode("Node8");
+			System.Windows.Forms.TreeNode treeNode8 = new System.Windows.Forms.TreeNode("Node9");
+			System.Windows.Forms.TreeNode treeNode9 = new System.Windows.Forms.TreeNode("Node2", new System.Windows.Forms.TreeNode[] {
+            treeNode7,
+            treeNode8});
+			System.Windows.Forms.TreeNode treeNode10 = new System.Windows.Forms.TreeNode("Node10");
+			System.Windows.Forms.TreeNode treeNode11 = new System.Windows.Forms.TreeNode("Node11");
+			System.Windows.Forms.TreeNode treeNode12 = new System.Windows.Forms.TreeNode("Node3", new System.Windows.Forms.TreeNode[] {
+            treeNode10,
+            treeNode11});
+			this.tabControl1 = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.treeView1 = new System.Windows.Forms.TreeView();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.treeView2 = new System.Windows.Forms.TreeView();
+			this.tabControl1.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPage2.SuspendLayout();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// tabControl1
+			// 
+			this.tabControl1.Controls.Add(this.tabPage1);
+			this.tabControl1.Controls.Add(this.tabPage2);
+			this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.tabControl1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.tabControl1.Location = new System.Drawing.Point(0, 0);
+			this.tabControl1.Name = "tabControl1";
+			this.tabControl1.SelectedIndex = 0;
+			this.tabControl1.Size = new System.Drawing.Size(663, 487);
+			this.tabControl1.TabIndex = 0;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.treeView2);
+			this.tabPage1.Controls.Add(this.tableLayoutPanel1);
+			this.tabPage1.Location = new System.Drawing.Point(4, 30);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(655, 453);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "数据库设置";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Controls.Add(this.treeView1);
+			this.tabPage2.Location = new System.Drawing.Point(4, 30);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(620, 410);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// panel1
+			// 
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(0, 487);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(663, 39);
+			this.panel1.TabIndex = 1;
+			// 
+			// treeView1
+			// 
+			this.treeView1.LabelEdit = true;
+			this.treeView1.Location = new System.Drawing.Point(250, 161);
+			this.treeView1.Name = "treeView1";
+			treeNode13.Name = "Node4";
+			treeNode13.Text = "Node4";
+			treeNode14.Name = "Node5";
+			treeNode14.Text = "Node5";
+			treeNode15.Name = "Node0";
+			treeNode15.Text = "Node0";
+			treeNode16.Name = "Node6";
+			treeNode16.Text = "Node6";
+			treeNode17.Name = "Node7";
+			treeNode17.Text = "Node7";
+			treeNode18.Name = "Node1";
+			treeNode18.Text = "Node1";
+			treeNode19.Name = "Node8";
+			treeNode19.Text = "Node8";
+			treeNode20.Name = "Node9";
+			treeNode20.Text = "Node9";
+			treeNode21.Name = "Node2";
+			treeNode21.Text = "Node2";
+			treeNode22.Name = "Node10";
+			treeNode22.Text = "Node10";
+			treeNode23.Name = "Node11";
+			treeNode23.Text = "Node11";
+			treeNode24.Name = "Node3";
+			treeNode24.Text = "Node3";
+			this.treeView1.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode15,
+            treeNode18,
+            treeNode21,
+            treeNode24});
+			this.treeView1.Size = new System.Drawing.Size(148, 97);
+			this.treeView1.TabIndex = 1;
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 24.02402F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75.97598F));
+			this.tableLayoutPanel1.Controls.Add(this.textBox3, 1, 2);
+			this.tableLayoutPanel1.Controls.Add(this.textBox2, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label1, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label3, 0, 2);
+			this.tableLayoutPanel1.Controls.Add(this.textBox1, 1, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(8, 21);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 3;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(359, 157);
+			this.tableLayoutPanel1.TabIndex = 1;
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(89, 107);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(214, 29);
+			this.textBox3.TabIndex = 5;
+			this.textBox3.UseSystemPasswordChar = true;
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(89, 55);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(214, 29);
+			this.textBox2.TabIndex = 4;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(3, 6);
+			this.label1.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(74, 21);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "服务器：";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 58);
+			this.label2.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(74, 21);
+			this.label2.TabIndex = 1;
+			this.label2.Text = "用户名：";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 110);
+			this.label3.Margin = new System.Windows.Forms.Padding(3, 6, 3, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(73, 21);
+			this.label3.TabIndex = 2;
+			this.label3.Text = "密   码：";
+			// 
+			// textBox1
+			// 
+			this.textBox1.BackColor = System.Drawing.Color.White;
+			this.textBox1.Location = new System.Drawing.Point(89, 3);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(214, 29);
+			this.textBox1.TabIndex = 3;
+			// 
+			// treeView2
+			// 
+			this.treeView2.LabelEdit = true;
+			this.treeView2.Location = new System.Drawing.Point(6, 195);
+			this.treeView2.Name = "treeView2";
+			treeNode1.Name = "Node4";
+			treeNode1.Text = "Node4";
+			treeNode2.Name = "Node5";
+			treeNode2.Text = "Node5";
+			treeNode3.Name = "Node0";
+			treeNode3.Text = "Node0";
+			treeNode4.Name = "Node6";
+			treeNode4.Text = "Node6";
+			treeNode5.Name = "Node7";
+			treeNode5.Text = "Node7";
+			treeNode6.Name = "Node1";
+			treeNode6.Text = "Node1";
+			treeNode7.Name = "Node8";
+			treeNode7.Text = "Node8";
+			treeNode8.Name = "Node9";
+			treeNode8.Text = "Node9";
+			treeNode9.Name = "Node2";
+			treeNode9.Text = "Node2";
+			treeNode10.Name = "Node10";
+			treeNode10.Text = "Node10";
+			treeNode11.Name = "Node11";
+			treeNode11.Text = "Node11";
+			treeNode12.Name = "Node3";
+			treeNode12.Text = "Node3";
+			this.treeView2.Nodes.AddRange(new System.Windows.Forms.TreeNode[] {
+            treeNode3,
+            treeNode6,
+            treeNode9,
+            treeNode12});
+			this.treeView2.Size = new System.Drawing.Size(146, 241);
+			this.treeView2.TabIndex = 2;
+			// 
+			// SystemConfigForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(663, 526);
+			this.Controls.Add(this.tabControl1);
+			this.Controls.Add(this.panel1);
+			this.Name = "SystemConfigForm";
+			this.Text = "系统配置";
+			this.tabControl1.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPage2.ResumeLayout(false);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TabControl tabControl1;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.TreeView treeView1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TextBox textBox3;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.TreeView treeView2;
+	}
+}
