e546430bbb628c91f50f527f32242ca6f5802abc
cranezhang <qzswjvv@sina.com>
2016-05-12 22:15:03 +0800

    Add Admin check when click SystemConfig

---
+++ MRS/AdminValidationForm.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace MRS
+{
+    partial class AdminValidationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_OK = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_OK
+            // 
+            this.btn_OK.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_OK.Location = new System.Drawing.Point(93, 88);
+            this.btn_OK.Name = "btn_OK";
+            this.btn_OK.Size = new System.Drawing.Size(75, 34);
+            this.btn_OK.TabIndex = 0;
+            this.btn_OK.Text = "确定";
+            this.btn_OK.UseVisualStyleBackColor = true;
+            this.btn_OK.Click += new System.EventHandler(this.btn_OK_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btn_Cancel.Location = new System.Drawing.Point(184, 88);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 34);
+            this.btn_Cancel.TabIndex = 1;
+            this.btn_Cancel.Text = "取消";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textBox1.Location = new System.Drawing.Point(46, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 29);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.UseSystemPasswordChar = true;
+            // 
+            // AdminValidationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 123);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_Cancel);
+            this.Controls.Add(this.btn_OK);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AdminValidationForm";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "请输入系统管理员密码";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_OK;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
--- MRS/MainForm.cs
+++ MRS/MainForm.cs
@@ -13,14 +13,14 @@ using MRS.DataCacheManager;
 
 namespace MRS
 {
-	public partial class MainForm : Form
-	{
+    public partial class MainForm : Form
+    {
         private IPatientPresenter patientPresenter;
         private ITemplatePresenter templatePresenter;
 
-		public MainForm()
-		{
-			InitializeComponent();
+        public MainForm()
+        {
+            InitializeComponent();
 
 
             patientPresenter = new PatientPresenter();
@@ -30,7 +30,7 @@ namespace MRS
 
             var patients = patientPresenter.GetPatientsByName();
             var templates = templatePresenter.GetTemplates();
-		}
+        }
 
         private void InitilizeCache()
         {
@@ -38,76 +38,82 @@ namespace MRS
             cacheManager.InitilizeDataCache();
         }
 
-		private void btn_LoadTemplate_Click(object sender, EventArgs e)
-		{
-			LoadTemplate form = new LoadTemplate();
-			form.ShowDialog();
-		}
+        private void btn_LoadTemplate_Click(object sender, EventArgs e)
+        {
+            LoadTemplate form = new LoadTemplate();
+            form.ShowDialog();
+        }
 
-		private void btn_SaveTemplate_Click(object sender, EventArgs e)
-		{
-			SaveTemplate form = new SaveTemplate();
-			form.ShowDialog();
-		}
+        private void btn_SaveTemplate_Click(object sender, EventArgs e)
+        {
+            SaveTemplate form = new SaveTemplate();
+            form.ShowDialog();
+        }
 
-		private void btn_ManageTemplate_Click(object sender, EventArgs e)
-		{
-			ManageTemplate form = new ManageTemplate();
-			form.ShowDialog();
-		}
+        private void btn_ManageTemplate_Click(object sender, EventArgs e)
+        {
+            ManageTemplate form = new ManageTemplate();
+            form.ShowDialog();
+        }
 
-		private void btn_SaveRecord_Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("保存病历");
-		}
+        private void btn_SaveRecord_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("保存病历");
+        }
 
-		private void btn_ManageData_Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("辅检数据");
-		}
+        private void btn_ManageData_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("辅检数据");
+        }
 
-		private void btn_Eval_Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("质控评分");
-		}
+        private void btn_Eval_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("质控评分");
+        }
 
-		private void btn_Validate_Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("病历校验");
-		}
+        private void btn_Validate_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("病历校验");
+        }
 
-		private void btn_ElecSignature_Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("电子签名");
-		}
+        private void btn_ElecSignature_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("电子签名");
+        }
 
-		private void btn_Finalize_Click(object sender, EventArgs e)
-		{
-			MessageBox.Show("档案归档");
-		}
+        private void btn_Finalize_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("档案归档");
+        }
 
-		private void btn_Configure_Click(object sender, EventArgs e)
-		{
-			UserConfigForm form = new UserConfigForm();
-			form.ShowDialog();
-		}
+        private void btn_Configure_Click(object sender, EventArgs e)
+        {
+            UserConfigForm form = new UserConfigForm();
+            form.ShowDialog();
+        }
 
-		private void btn_Search_Click(object sender, EventArgs e)
-		{
-			SearchPatient form = new SearchPatient();
-			form.ShowDialog();
-		}
+        private void btn_Search_Click(object sender, EventArgs e)
+        {
+            SearchPatient form = new SearchPatient();
+            form.ShowDialog();
+        }
 
-		private void btn_History_Click(object sender, EventArgs e)
-		{
-			SearchPatient form = new SearchPatient();
-			form.ShowDialog();
-		}
+        private void btn_History_Click(object sender, EventArgs e)
+        {
+            SearchPatient form = new SearchPatient();
+            form.ShowDialog();
+        }
 
-		private void btn_SystemConfigure_Click(object sender, EventArgs e)
-		{
-            SystemConfigForm form = new SystemConfigForm();
-            form.Show();
-		}
-	}
+        private void btn_SystemConfigure_Click(object sender, EventArgs e)
+        {
+            AdminValidationForm form = new AdminValidationForm();
+            if (form.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                SystemConfigForm form1 = new SystemConfigForm();
+                form1.Show();
+            }
+
+
+        }
+    }
 }
