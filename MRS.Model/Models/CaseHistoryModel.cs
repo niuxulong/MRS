5003980ec367b278b4076ebde0f55c06df133ddf
niuxulong <niu-xu-long@163.com>
2016-05-21 18:41:14 +0800

    s

--- MRS.Model/Models/CaseHistoryModel.cs
+++ MRS.Model/Models/CaseHistoryModel.cs
@@ -6,9 +6,6 @@ using MRS.Model.Interfaces;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MRS.Model.Models
 {
